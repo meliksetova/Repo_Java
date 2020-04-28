738d06e3c03ac048875b840262504f7cf0589284
meliksetova <64248866+meliksetova@users.noreply.github.com>
2020-04-28 00:57:18 -0400

    Add files via upload

---
+++ Basics.java
@@ -0,0 +1,14 @@
+package Homework;
+
+public class Basics {
+    public static void main (String[] args) {
+        int d = 5;
+        System.out.println(d++);  //5
+        System.out.println(d++);   //6
+        System.out.println(++d);  //8
+        System.out.println(d);    //8
+        System.out.println(--d);  //7
+        System.out.println(d);    //7
+        System.out.println(d++);   //7
+    }    }
+
